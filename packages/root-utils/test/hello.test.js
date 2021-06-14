1d67927c7873369ba585c3a6eedc8ee36ca26cfb
Sam Lord <hello@samlord.me>
2021-06-14 15:13:21 +0100

    Added tests to root-utils package
    no issue
    Both tests work when called using `yarn test` in the module itself
    and at the monorepo level. Tests were based on the output of the
    functions currently, since these are known-good functions

--- packages/root-utils/test/hello.test.js
+++ packages/root-utils/test/hello.test.js
@@ -1,10 +1,18 @@
 // Switch these lines once there are useful utils
 // const testUtils = require('./utils');
 require('./utils');
+const {getCallerRoot, getProcessRoot} = require('../index');
 
-describe('Hello world', function () {
-    it('Runs a test', function () {
-        // TODO: Write me!
-        'hello'.should.eql('hello');
+describe('getCallerRoot', function () {
+    it('Gets the root directory of the caller', function () {
+        // mocha calls the test function calls getCallerRoot
+        getCallerRoot().should.endWith('mocha');
+    });
+});
+
+describe('getProcessRoot', function () {
+    it('Gets the root directory of the process', function () {
+        // root-utils is the main module of the process
+        getProcessRoot().should.endWith('root-utils');
     });
 });
