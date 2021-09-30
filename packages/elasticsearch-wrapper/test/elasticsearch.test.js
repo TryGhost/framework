9c2c47aefd3838eccca6891359b1fe1e14365411
Sam Lord <hello@samlord.me>
2021-09-30 16:46:03 +0100

    Add ElasticSearch wrapper library

---
+++ packages/elasticsearch-wrapper/test/elasticsearch.test.js
@@ -0,0 +1,85 @@
+const sinon = require('sinon');
+const should = require('should');
+const sandbox = sinon.createSandbox();
+
+const {Client} = require('@elastic/elasticsearch');
+const ElasticSearch = require('../index');
+
+const testClientConfig = {
+    node: 'http://test-elastic-client',
+    auth: {
+        username: 'user',
+        password: 'pass'
+    }
+};
+
+const indexConfig = {
+    index: 'test-index',
+    pipeline: 'test-pipeline'
+};
+
+describe('ElasticSearch', function () {
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('Processes client configuration', function () {
+        const es = new ElasticSearch(testClientConfig);
+
+        should.exist(es.client);
+        should.exist(es.client.index);
+    });
+
+    it('Processes index configuration', async function () {
+        sandbox.stub(Client.prototype, 'index').callsFake((data) => {
+            should.exist(data.body);
+            should.deepEqual(data.body, testBody);
+            should.exist(data.index);
+            should.equal(data.index, indexConfig.index);
+            should.exist(data.pipeline);
+            should.equal(data.pipeline, indexConfig.pipeline);
+        });
+
+        const testBody = {
+            message: 'Test data!'
+        };
+
+        const es = new ElasticSearch(testClientConfig);
+
+        await es.index(testBody, indexConfig);
+
+        Client.prototype.index.called.should.eql(true);
+    });
+
+    it('Calls index on valid events', async function () {
+        const es = new ElasticSearch(testClientConfig);
+        const indexStub = sandbox.stub(Client.prototype, 'index');
+
+        await es.index({
+            message: 'test!s'
+        }, indexConfig);
+        
+        indexStub.callCount.should.equal(1);
+    });
+
+    it('Does not index invalid events', async function () {
+        const es = new ElasticSearch(testClientConfig);
+        const indexStub = sandbox.stub(Client.prototype, 'index');
+
+        await es.index('not an object', indexConfig);
+        
+        indexStub.callCount.should.equal(0);
+    });
+
+    it('Uses index config as a string', async function () {
+        const es = new ElasticSearch(testClientConfig);
+        sandbox.stub(Client.prototype, 'index').callsFake((data) => {
+            should.exist(data.index);
+            should.equal(data.index, indexConfig.index);
+        });
+
+        await es.index({
+            message: 'Test data'
+        }, indexConfig.index);
+    });
+});
