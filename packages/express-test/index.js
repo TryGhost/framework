412cd2219a1eb1e78978293b158900323f0e2776
Naz <nazargargol@gmail.com>
2022-02-21 22:03:55 +0700

    Exposed jest-snapshot API through express-test
    refs https://github.com/TryGhost/Toolbox/issues/215
    
    - Work on Content API has been stalled because of reoccuring issue with @tryghost/jest-snapshot package version missmatch between client (Ghost) and express-test's version.
    - Exposing all needed methods through express-test to the clients avoids the problem of different versions and allows for singleton pattern around snapshotManager to function properly

--- packages/express-test/index.js
+++ packages/express-test/index.js
@@ -1,1 +1,9 @@
 module.exports = require('./lib/agent');
+
+// NOTE: exposing jest-snapshot as a part of express-test to avoid
+//       version missmatching on the client side
+module.exports.snapshot = {
+    mochaHooks: require('@tryghost/jest-snapshot').mochaHooks,
+    any: require('@tryghost/jest-snapshot').any,
+    stringMatching: require('@tryghost/jest-snapshot').stringMatching
+};
