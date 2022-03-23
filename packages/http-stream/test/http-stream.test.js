f47f052bbfcbd1b45c8853883610e48919347429
Joe Grigg <joe@ghost.org>
2022-03-23 10:33:43 +0000

    Added a simple http-stream package
    refs https://github.com/TryGhost/BlackPearl/issues/1378
    
    This package was created to be used with @tryghost/logging in the future.
    
    It might not actually be the solution we use for the referenced issue but as it was working I have published it anyway as it might come in use in the future.

---
+++ packages/http-stream/test/http-stream.test.js
@@ -0,0 +1,38 @@
+require('should');
+const HttpStream = require('../index');
+
+const testConfig = {
+    url: 'http://localhost:3000'
+};
+
+describe('HttpStream', function () {
+    describe('write', function () {
+        let server;
+        beforeEach(function () {
+            const express = require('express');
+            const app = express();
+            app.use(express.json());
+            app.post('*', (req, res) => {
+                res.json(req.body);
+            });
+            server = app.listen(3000);
+        });
+        afterEach(function () {
+            server.close();
+        });
+
+        it('return false when string passed', async function () {
+            let stream = new HttpStream(testConfig);
+            let res = await stream.write('this shouldnt work');
+            (res).should.eql(false);
+        });
+
+        it('be successful when object passed', async function () {
+            let stream = new HttpStream(testConfig);
+            let body = {this: 'should work'};
+            let res = await stream.write(body);
+            (res.statusCode).should.eql(200);
+            (JSON.parse(res.body)).should.eql(body);
+        });
+    });
+});
