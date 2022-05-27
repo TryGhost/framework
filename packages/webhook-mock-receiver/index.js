d95fb2417fcf43222921318fb4d23eca84557481
Naz <nazargargol@gmail.com>
2022-05-27 12:43:21 +0800

    Bootstrapped webhook mock receiver package
    refs https://github.com/TryGhost/Toolbox/issues/320
    
    - Slimer template minus the "should" fluff to build up upon

---
+++ packages/webhook-mock-receiver/index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/webhook-mock-receiver');
---
+++ packages/webhook-mock-receiver/test/webhook-mock-receiver.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Webhook Mock Receiver', function () {
+    it('Runs a test', function () {
+        // TODO: Write me!
+        assert.equal('hello', 'hello');
+    });
+});
