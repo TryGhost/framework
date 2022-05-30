139939b775d27a6ad905fbf32492e4d6e02acc73
Naz <nazargargol@gmail.com>
2022-05-30 14:16:06 +0800

    Added webhook mock receiver base implementaiton
    refs https://github.com/TryGhost/Toolbox/issues/320
    
    - The mock receiver allows to mock, record, and snapshot-match outgoing requests. The main purpose of the module is to be able to test webhook implementation in Ghost.
    - This is a bare minimum implementation to allow checking a single webhook request body. Due to time restriction the  rest of features (like header matching) should be implemented when there's more time dedicated to webhook testing.

---
+++ packages/webhook-mock-receiver/lib/webhook-mock-receiver.js
@@ -0,0 +1,58 @@
+const util = require('util');
+const {URL} = require('url');
+const nock = require('nock');
+const setTimeoutPromise = util.promisify(setTimeout);
+
+class WebhookMockReceiver {
+    constructor({snapshotManager}) {
+        this.bodyResponse;
+        this.receiver;
+        this.snapshotManager = snapshotManager;
+        this.recordBodyResponse = this.recordBodyResponse.bind(this);
+    }
+
+    recordBodyResponse(body) {
+        this.bodyResponse = {body};
+
+        // let the nock continue with the response
+        return true;
+    }
+
+    /**
+     *
+     * @param {String} url endpoint URL to be mocked
+     * @returns WebhookMockReceiver
+     */
+    mock(url) {
+        const parsedURL = new URL(url);
+
+        this.receiver = nock(parsedURL.origin)
+            .post(parsedURL.pathname, this.recordBodyResponse)
+            .reply(200, {status: 'OK'});
+
+        return this;
+    }
+
+    reset() {
+        nock.cleanAll();
+        this.receiver = undefined;
+        this.bodyResponse = undefined;
+    }
+
+    async matchBodySnapshot(properties = {}) {
+        // @TODO: figure out a better waiting mechanism here, don't allow it to hang forever
+        while (!this.receiver.isDone()) {
+            await setTimeoutPromise(10);
+        }
+
+        let assertion = {
+            properties: properties,
+            field: 'body',
+            type: 'body'
+        };
+
+        this.snapshotManager.assertSnapshot(this.bodyResponse, assertion);
+    }
+}
+
+module.exports = WebhookMockReceiver;
--- packages/webhook-mock-receiver/test/webhook-mock-receiver.test.js
+++ packages/webhook-mock-receiver/test/webhook-mock-receiver.test.js
@@ -1,8 +1,124 @@
 const assert = require('assert');
+const got = require('got');
+const sinon = require('sinon');
+
+const WebhookMockReceiver = require('../');
 
 describe('Webhook Mock Receiver', function () {
-    it('Runs a test', function () {
-        // TODO: Write me!
-        assert.equal('hello', 'hello');
+    let snapshotManager;
+    let webhookMockReceiver;
+    const webhookURL = 'https://test-webhook-receiver.com/webhook';
+
+    before(function () {
+        snapshotManager = {
+            assertSnapshot: sinon.spy()
+        };
+        webhookMockReceiver = new WebhookMockReceiver({
+            snapshotManager
+        });
+    });
+
+    afterEach(function () {
+        sinon.reset();
+        webhookMockReceiver.reset();
+    });
+
+    describe('recordBodyResponse', function () {
+        it('saves the payload', function () {
+            const result = webhookMockReceiver.recordBodyResponse({foo: 'bar'});
+
+            assert.equal(result, true);
+            assert.deepEqual(webhookMockReceiver.bodyResponse, {
+                body: {
+                    foo: 'bar'
+                }
+            });
+        });
+    });
+
+    describe('mock', function () {
+        it('created a mock request receiver base on url', async function () {
+            webhookMockReceiver.mock(webhookURL);
+
+            assert.notEqual(webhookMockReceiver.receiver, undefined);
+
+            assert.equal(webhookMockReceiver.receiver.isDone(), false);
+
+            await got.post(webhookURL, {
+                body: {
+                    avocado: 'toast'
+                },
+                json: true
+            });
+
+            assert.equal(webhookMockReceiver.receiver.isDone(), true);
+            assert.deepEqual(webhookMockReceiver.bodyResponse, {
+                body: {
+                    avocado: 'toast'
+                }
+            });
+        });
+    });
+
+    describe('reset', function () {
+        it('resets the default state of the mock receiver', async function () {
+            webhookMockReceiver.mock(webhookURL);
+            await got.post(webhookURL, {
+                avocado: 'toast'
+            });
+
+            assert.notEqual(webhookMockReceiver.receiver, undefined);
+            assert.notEqual(webhookMockReceiver.bodyResponse, undefined);
+
+            webhookMockReceiver.reset();
+
+            assert.equal(webhookMockReceiver.receiver, undefined);
+            assert.equal(webhookMockReceiver.bodyResponse, undefined);
+        });
+    });
+
+    describe('matchBodySnapshot', function () {
+        it('checks the request payload', async function () {
+            webhookMockReceiver.mock(webhookURL);
+            await got.post(webhookURL, {
+                body: {
+                    avocado: 'toast'
+                },
+                json: true
+            });
+
+            await webhookMockReceiver.matchBodySnapshot();
+
+            assert.equal(snapshotManager.assertSnapshot.calledOnce, true);
+            assert.deepEqual(snapshotManager.assertSnapshot.args[0][0], {
+                body: {
+                    avocado: 'toast'
+                }
+            });
+        });
+
+        it('waits for the request completeion before checking the request payload', async function () {
+            webhookMockReceiver.mock(webhookURL);
+
+            // shoot a request with a delay simulating request completion delay
+            // 10 is the delay that's used now to recheck for completed request
+            setTimeout(() => {
+                got.post(webhookURL, {
+                    body: {
+                        avocado: 'toast'
+                    },
+                    json: true
+                });
+            }, (10 + 1));
+
+            await webhookMockReceiver.matchBodySnapshot();
+
+            assert.equal(snapshotManager.assertSnapshot.calledOnce, true);
+            assert.deepEqual(snapshotManager.assertSnapshot.args[0][0], {
+                body: {
+                    avocado: 'toast'
+                }
+            });
+        });
     });
 });
