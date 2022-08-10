b6d9aa41677a52078d1aaf848e21f29f40d4457c
Simon Backx <simon@ghost.org>
2022-08-10 14:37:48 +0200

    Decoupled bookshelf-include-count from models and added nested relations support (#75)
    refs https://github.com/TryGhost/Team/issues/1723
    
    1. Updated to also work for nested relations
    We can no longer use fetch and fetchAll to inject and update the counts. The problem with using fetch(All) is that these methods are not called for 'eager' loaded relations.
    
    E.g., when loading comments with 'replies.count.likes' in withRelated. In the past, this would have caused an internal error. Because when Bookshelf would fetch the 'replies' relation, it wouldn't run the counts script where it needed to remove 'count.likes' from withRelated in order to prevent fetching it.
    
    A better alternative is the sync method of Model and Collection. This method is called to do any database operation, also eager relations. Because of the way the sync method is called, we need to modify the withRelated relation while keeping the reference to the array (because otherwise the sync method doesn't pass the updated options to following methods).
    
    2. Updated to keep the counts after saving a model (crud.edit didn't return the counts before)
    Ghost core removes not permitted attributes, such as count__likes from a model while saving a model. To fix this, we make sure we keep those attributes after a save. This makes it easier to return counts in the API without having to refetch a model with all the relations.
    
    3. Removed all the queries from the bookshelf-include-count package
    You now need to define the count queries in models by implementing the countRelations static method.
    
    All existing queries have been moved to https://github.com/TryGhost/Ghost/pull/15206
    
    E.g.,
    ```
    countRelations() {
        return {
            replies(modelOrCollection) {
                modelOrCollection.query('columns', 'comments.*', (qb) => {
                    qb.count('replies.id')
                        .from('comments AS replies')
                        .whereRaw('replies.parent_id = comments.id')
                        .as('count__replies');
                });
            },
            likes(modelOrCollection) {
                modelOrCollection.query('columns', 'comments.*', (qb) => {
                    qb.count('comment_likes.id')
                        .from('comment_likes')
                        .whereRaw('comment_likes.comment_id = comments.id')
                        .as('count__likes');
                });
            },
            liked(modelOrCollection, options) {
                modelOrCollection.query('columns', 'comments.*', (qb) => {
                    if (options.context && options.context.member && options.context.member.id) {
                        qb.count('comment_likes.id')
                            .from('comment_likes')
                            .whereRaw('comment_likes.comment_id = comments.id')
                            .where('comment_likes.member_id', options.context.member.id)
                            .as('count__liked');
                        return;
                    }
    
                    // Return zero
                    qb.select(ghostBookshelf.knex.raw('0')).as('count__liked');
                });
            }
        };
    },
    ```

--- packages/bookshelf-include-count/lib/bookshelf-include-count.js
+++ packages/bookshelf-include-count/lib/bookshelf-include-count.js
@@ -7,142 +7,73 @@ const _ = require('lodash');
  */
 module.exports = function (Bookshelf) {
     const modelProto = Bookshelf.Model.prototype;
-    const countQueryBuilder = {
-        /**
-         * Ideally, these configs should exist on the model and be much more dynamic
-         * In reality, there's no point fixing this
-         */
-        tags: {
-            posts: function addPostCountToTags(model, options) {
-                model.query('columns', 'tags.*', function (qb) {
-                    qb.count('posts.id')
-                        .from('posts')
-                        .leftOuterJoin('posts_tags', 'posts.id', 'posts_tags.post_id')
-                        .whereRaw('posts_tags.tag_id = tags.id')
-                        .as('count__posts');
 
-                    if (options.context && options.context.public) {
-                        // @TODO use the filter behavior for posts
-                        qb.andWhere('posts.type', '=', 'post');
-                        qb.andWhere('posts.status', '=', 'published');
-                    }
-                });
-            }
-        },
-        labels: {
-            members: function addMemberCountToLabels(model) {
-                model.query('columns', 'labels.*', function (qb) {
-                    qb.count('members.id')
-                        .from('members')
-                        .leftOuterJoin('members_labels', 'members.id', 'members_labels.member_id')
-                        .whereRaw('members_labels.label_id = labels.id')
-                        .as('count__members');
-                });
-            }
-        },
-        users: {
-            posts: function addPostCountToUsers(model, options) {
-                model.query('columns', 'users.*', function (qb) {
-                    qb.count('posts.id')
-                        .from('posts')
-                        .join('posts_authors', 'posts.id', 'posts_authors.post_id')
-                        .whereRaw('posts_authors.author_id = users.id')
-                        .as('count__posts');
+    const addCounts = function (options) {
+        if (!options) {
+            return;
+        }
+        if (!options.withRelated) {
+            return;
+        }
 
-                    if (options.context && options.context.public) {
-                        // @TODO use the filter behavior for posts
-                        qb.andWhere('posts.type', '=', 'post');
-                        qb.andWhere('posts.status', '=', 'published');
-                    }
-                });
-            }
-        },
-        /* Speculative */
-        channels: {
-            posts: function addPostCountToChannels(model, options) {
-                model.query('columns', 'channels.*', function (qb) {
-                    qb.count('posts.id')
-                        .from('posts')
-                        .leftOuterJoin('posts_channels', 'posts.id', 'posts_channels.post_id')
-                        .whereRaw('posts_channels.channel_id = channels.id')
-                        .as('count__posts');
+        // Helper methods
+        // withRelated can be an object or an array of strings. We need to support handling both representations.
+        // ['user', 'replies']
+        // OR
+        // [
+        //    {'user': function() {} }
+        // ]
 
-                    if (options.context && options.context.public) {
-                        // @TODO use the filter behavior for posts
-                        qb.andWhere('posts.type', '=', 'post');
-                        qb.andWhere('posts.status', '=', 'published');
+        function hasWithRelated(key) {
+            for (const item of options.withRelated) {
+                if (typeof item !== 'string') {
+                    if (item[key] !== undefined) {
+                        return true;
                     }
-                });
+                }
+                if (item === key) {
+                    return true;
+                }
             }
-        },
-        newsletters: {
-            posts: function addPostCountToNewsletters(model) {
-                model.query('columns', 'newsletters.*', function (qb) {
-                    qb.count('posts.id')
-                        .from('posts')
-                        .whereRaw('posts.newsletter_id = newsletters.id')
-                        .as('count__posts');
-                });
-            },
-            members: function addMemberCountToNewsletters(model) {
-                model.query('columns', 'newsletters.*', function (qb) {
-                    qb.count('members_newsletters.id')
-                        .from('members_newsletters')
-                        .whereRaw('members_newsletters.newsletter_id = newsletters.id')
-                        .as('count__members');
-                });
+            return false;
+        }
+
+        function removeWithRelated(key) {
+            // VERY IMPORTANT HERE:
+            // We need to keep the reference to the withRelated array and not create a new array
+            // This is required to make eager relations work correctly (otherwise the updated withRelated won't get passed further)
+            const newItems = options.withRelated.filter((item) => {
+                if (typeof item === 'string') {
+                    return item !== key;
+                }
+                return item[key] === undefined;
+            });
+            options.withRelated.splice(0, options.withRelated.length, ...newItems);
+        }
+
+        // This can run in both a model or in a collection
+        // We need access to the model's (optional) countRelations method.
+        let model = this.constructor;
+        if (this.model) {
+            model = this.model;
+        }
+
+        if (model.countRelations) {
+            const countRelations = model.countRelations();
+            for (const countRelation of Object.keys(countRelations)) {
+                if (hasWithRelated('count.' + countRelation)) {
+                    // remove post_count from withRelated
+                    removeWithRelated('count.' + countRelation);
+        
+                    // Call the query builder
+                    countRelations[countRelation](this, options);
+                }
             }
         }
     };
 
     const Model = Bookshelf.Model.extend({
-        addCounts: function (options) {
-            if (!options) {
-                return;
-            }
-
-            const tableName = _.result(this, 'tableName');
-
-            if (options.withRelated && options.withRelated.indexOf('count.posts') > -1) {
-                // remove post_count from withRelated
-                options.withRelated = _.pull([].concat(options.withRelated), 'count.posts');
-
-                // Call the query builder
-                countQueryBuilder[tableName].posts(this, options);
-            }
-            if (options.withRelated && options.withRelated.indexOf('count.members') > -1) {
-                // remove post_count from withRelated
-                options.withRelated = _.pull([].concat(options.withRelated), 'count.members');
-
-                // Call the query builder
-                countQueryBuilder[tableName].members(this, options);
-            }
-        },
-        fetch: function () {
-            this.addCounts.apply(this, arguments);
-
-            // Useful when debugging no. database queries, GQL, etc
-            // To output this, use DEBUG=ghost:*,ghost-query
-            if (_debug.enabled('ghost-query')) {
-                debug('QUERY', this.query().toQuery());
-            }
-
-            // Call parent fetch
-            return modelProto.fetch.apply(this, arguments);
-        },
-        fetchAll: function () {
-            this.addCounts.apply(this, arguments);
-
-            // Useful when debugging no. database queries, GQL, etc
-            // To output this, use DEBUG=ghost:*,ghost-query
-            if (_debug.enabled('ghost-query')) {
-                debug('QUERY', this.query().toQuery());
-            }
-
-            // Call parent fetchAll
-            return modelProto.fetchAll.apply(this, arguments);
-        },
-
+        addCounts,
         serialize: function serialize(options) {
             const attrs = modelProto.serialize.call(this, options);
             const countRegex = /^(count)(__)(.*)$/;
@@ -157,8 +88,69 @@ module.exports = function (Bookshelf) {
             });
 
             return attrs;
+        },
+
+        /**
+         * Instead of adding the counts in .fetch and .fetchAll,
+         * we need to do it in sync because Bookshelf doesn't call fetch for eagerRelations
+         * E.g. when trying to load counts on replies.count.likes, we wouldn't get an opportunity to load the counts on the replies relation.
+         */
+        sync: function (options) {
+            if (!options.method || (options.method !== 'insert' && options.method !== 'update')) {
+                this.addCounts.apply(this, arguments);
+            }
+
+            if (_debug.enabled('ghost-query')) {
+                debug('QUERY', this.query().toQuery());
+            }
+
+            // Call parent fetchAll
+            return modelProto.sync.apply(this, arguments);
+        },
+
+        // Warning: Make sure this method always returns a Bluebird Promise (modelProto.save.apply does, so returning that is fine)
+        save: function save() {
+            // the count__ variables are not 'permitted' and will get removed after a save
+            // so this will make sure they are kept alive after a save (unless they are also still available after the save)
+
+            const savedAttributes = {};
+            const countRegex = /^(count)(__)(.*)$/;
+
+            for (const key of Object.keys(this.attributes)) {
+                const match = key.match(countRegex);
+                if (match) {
+                    savedAttributes[key] = this.attributes[key];
+                }
+            }
+            
+            return modelProto.save.apply(this, arguments).then((t) => {
+                // Set savedAttributes, but keep count__ variables if they stayed inside this.attributes
+                if (savedAttributes) {
+                    Object.assign(this.attributes, savedAttributes, this.attributes);
+                }
+                return t;
+            });
         }
     });
 
     Bookshelf.Model = Model;
+
+    const collectionProto = Bookshelf.Collection.prototype;
+
+    const Collection = Bookshelf.Collection.extend({
+        addCounts,
+        sync: function () {
+            // For now, only apply this for eager loaded collections
+            this.addCounts.apply(this, arguments);
+
+            if (_debug.enabled('ghost-query')) {
+                debug('QUERY', this.query().toQuery());
+            }
+
+            // Call parent fetchAll
+            return collectionProto.sync.apply(this, arguments);
+        }
+    });
+
+    Bookshelf.Collection = Collection;
 };
