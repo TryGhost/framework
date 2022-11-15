efe9b332826f146ffcbc5e9d23073579f4e99c0e
Hannah Wolfe <github.erisds@gmail.com>
2022-11-15 17:20:38 +0000

    Fixed eslint error
    - I was originally going to disable the rule globally for this packge, by setting it to off in the .eslintrc.js file
    - However, the no-restricted-syntax rule covers more than one thing and may be extended in future.
    - Therefore, as there's only one affected line, I decided to use an eslint disable line comment to cover just this exact case for now

--- packages/promise/lib/pool.js
+++ packages/promise/lib/pool.js
@@ -2,17 +2,17 @@
  * Promise Pool
  *
  * The pool will execute all the tasks with maxConcurrent tasks
- * running simoultaneously at most. 
+ * running simoultaneously at most.
  */
 
 async function pool(tasks, maxConcurrent) {
     if (maxConcurrent < 1) {
-        throw new Error('Must set at least 1 concurrent workers');
+        throw new Error('Must set at least 1 concurrent workers'); // eslint-disable-line no-restricted-syntax
     }
 
     const taskIterator = tasks.entries();
     const results = [];
- 
+
     const workers = Array(maxConcurrent).fill(taskIterator).map(
         async (workerIterator) => {
             for (let [index, task] of workerIterator) {
