60b57a58aab47652cf6bc2784a73e949270083e6
Naz <hi@nazavo.com>
2023-01-11 14:56:17 +0700

    Added matchHTMLSnapshot method to store html snapshots
    refs https://github.com/TryGhost/Toolbox/issues/499
    
    - Email mock receiver needs a way to store html content snapshots for outgoing emails - this is what "matchHTMLSnapshot" allows to do.
    - Caveat, when sending email is called up multiple times only the very first email snapshot is taken into account

--- packages/email-mock-receiver/lib/email-mock-receiver.js
+++ packages/email-mock-receiver/lib/email-mock-receiver.js
@@ -1,8 +1,61 @@
-class EmailMockReceiver {
-    #snapshotManager;
+const {AssertionError} = require('assert');
 
-    constructor({snapshotManager}) {
+class EmailMockReceiver {
+    #sendResponse;
+    #snapshotManager;
+    #snapshot = null;
+
+    constructor({snapshotManager, sendResponse = 'Mail is disabled'}) {
         this.#snapshotManager = snapshotManager;
+        this.#sendResponse = sendResponse;
+    }
+
+    /**
+     * Method mocking email sending logic
+     *
+     * @param {Object} message
+     * @param {string} message.subject - email subject
+     * @param {string} message.html - email content
+     * @param {string} message.to - email recipient address
+     * @param {string} [message.replyTo]
+     * @param {string} [message.from] - sender email address
+     * @param {string} [message.text] - text version of this message
+     */
+    async send({subject, html, to, replyTo, from, text}) {
+        if (this.#snapshot) {
+            return this.#sendResponse;
+        }
+
+        // store snapshot
+        this.#snapshot = {
+            subject,
+            html,
+            to,
+            from,
+            replyTo,
+            text
+        };
+
+        return this.#sendResponse;
+    }
+
+    matchHTMLSnapshot() {
+        const error = new AssertionError({});
+        let assertion = {
+            properties: null,
+            field: 'html',
+            error
+        };
+
+        this.#snapshotManager.assertSnapshot({
+            html: this.#snapshot.html
+        }, assertion);
+
+        return this;
+    }
+
+    reset() {
+        this.#snapshot = null;
     }
 }
 
--- packages/email-mock-receiver/test/email-mock-receiver.test.js
+++ packages/email-mock-receiver/test/email-mock-receiver.test.js
@@ -1,8 +1,52 @@
 const assert = require('assert');
+const sinon = require('sinon');
+
 const EmailMockReceiver = require('../index');
 
 describe('Email mock receiver', function () {
-    it('Runs a test', function () {
+    let snapshotManager;
+    let emailMockReceiver;
+
+    before(function () {
+        snapshotManager = {
+            assertSnapshot: sinon.spy()
+        };
+
+        emailMockReceiver = new EmailMockReceiver({snapshotManager});
+    });
+
+    afterEach(function () {
+        sinon.reset();
+        emailMockReceiver.reset();
+    });
+
+    it('Can initialize', function () {
         assert.ok(new EmailMockReceiver({snapshotManager: {}}));
+    });
+
+    describe('matchHTMLSnapshot', function () {
+        it('Can match primitive HTML snapshot', function () {
+            emailMockReceiver.send({html: '<div>test</div>'});
+            emailMockReceiver.matchHTMLSnapshot();
+
+            assert.equal(snapshotManager.assertSnapshot.calledOnce, true);
+            assert.deepEqual(snapshotManager.assertSnapshot.args[0][0], {
+                html: '<div>test</div>'
+            });
+        });
+
+        it('Can match first HTML snapshot with multiple send requests are executed', function () {
+            emailMockReceiver = new EmailMockReceiver({snapshotManager});
+
+            emailMockReceiver.send({html: '<div>test 1</div>'});
+            emailMockReceiver.send({html: '<div>test 2</div>'});
+
+            emailMockReceiver.matchHTMLSnapshot();
+
+            assert.equal(snapshotManager.assertSnapshot.calledOnce, true);
+            assert.deepEqual(snapshotManager.assertSnapshot.args[0][0], {
+                html: '<div>test 1</div>'
+            });
+        });
     });
 });
