b60f0e891f52345e1199c9791b04eddd9b53e49c
Naz <hi@nazavo.com>
2023-01-17 15:37:41 +0800

    Fixed typo

--- packages/express-test/index.js
+++ packages/express-test/index.js
@@ -1,7 +1,7 @@
 module.exports = require('./lib/agent');
 
 // NOTE: exposing jest-snapshot as a part of express-test to avoid
-//       version missmatching on the client side
+//       version mismatching on the client side
 module.exports.snapshot = {
     mochaHooks: require('@tryghost/jest-snapshot').mochaHooks,
     snapshotManager: require('@tryghost/jest-snapshot').snapshotManager,
