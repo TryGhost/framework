ea4af6006d34dacc427b9e9e91732f0e98294aa9
Jono Mingard <reason.koan@gmail.com>
2023-11-02 14:13:56 +0000

    Exported GhostError type

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,7 +1,9 @@
+import {GhostError} from './GhostError';
 import * as ghostErrors from './errors';
 import {deserialize, isGhostError, prepareStackForUser, serialize} from './utils';
 
 export * from './errors';
+export type {GhostError};
 export default ghostErrors;
 
 export const utils = {
