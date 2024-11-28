83e735b6a836143490177ccd1275ab25c255c273
Hannah Wolfe <github.erisds@gmail.com>
2024-11-28 15:53:21 +0000

    Added a realistic test case
    - Wrote a test that shows stepping through the behaviour of the framework works as expected

--- packages/jest-snapshot/test/jest-snapshot.test.js
+++ packages/jest-snapshot/test/jest-snapshot.test.js
@@ -59,4 +59,80 @@ describe('Jest Snapshot', function () {
             sinon.assert.calledOnce(resetRegistrySpy);
         });
     });
+
+    describe('framework expectations', function () {
+        // The expectation of this framework is that you wire up the beforeAll and beforeEach hooks
+        // and then call snapshotManager.match in your tests, either directly, via matchSnapshotAssertion
+        // or via the snapshotManager.assertSnapshot method.
+
+        // This test checks that match works as expected when using beforeAll and beforeEach
+        const testFile = 'test/framework-expectations-test.js';
+        const firstTestTitle = 'My first test';
+        const secondTestTitle = 'My second test';
+
+        it('snapshot matching works as expected when using beforeAll and beforeEach', function () {
+            // Setup some spies
+            const resetRegistrySpy = sinon.spy(snapshotTools.snapshotManager, 'resetRegistry');
+            const setCurrentTestSpy = sinon.spy(snapshotTools.snapshotManager, 'setCurrentTest');
+
+            const firstTest = {file: `${testFile}`, fullTitle: () => firstTestTitle, currentRetry: () => 0};
+            const secondTest = {file: `${testFile}`, fullTitle: () => secondTestTitle, currentRetry: () => 0};
+            const expectedResult = {foo: 'bar'};
+
+            let testResult;
+
+            // Call beforeAll - this should reset the registry
+            snapshotTools.mochaHooks.beforeAll();
+            sinon.assert.calledOnce(resetRegistrySpy);
+            assert.deepEqual(snapshotTools.snapshotManager.registry, {});
+
+            // Execute a test calling beforeEach and using match
+            snapshotTools.mochaHooks.beforeEach.call({currentTest: firstTest});
+            testResult = snapshotTools.snapshotManager.match(expectedResult);
+
+            // Check the test was called how we expected
+            sinon.assert.calledOnce(setCurrentTestSpy);
+            assert.deepEqual(setCurrentTestSpy.firstCall.args[0], {testPath: `${testFile}`, testTitle: firstTestTitle});
+            assert.equal(testResult.pass, true);
+
+            // Check the registry looks as expected
+            assert.deepEqual(snapshotTools.snapshotManager.registry, {
+                [`${testFile}`]: {
+                    [`${firstTestTitle}`]: 1
+                }
+            });
+
+            // Execute a second test
+            snapshotTools.mochaHooks.beforeEach.call({currentTest: secondTest});
+            testResult = snapshotTools.snapshotManager.match(expectedResult);
+
+            sinon.assert.calledTwice(setCurrentTestSpy);
+            assert.deepEqual(setCurrentTestSpy.secondCall.args[0], {testPath: `${testFile}`, testTitle: secondTestTitle});
+            assert.equal(testResult.pass, false);
+
+            // Check the registry looks as expected
+            assert.deepEqual(snapshotTools.snapshotManager.registry, {
+                [`${testFile}`]: {
+                    [`${firstTestTitle}`]: 1,
+                    [`${secondTestTitle}`]: 1
+                }
+            });
+
+            // Execute a third test, which is the second test duplicated
+            snapshotTools.mochaHooks.beforeEach.call({currentTest: secondTest});
+            testResult = snapshotTools.snapshotManager.match(expectedResult);
+
+            sinon.assert.calledThrice(setCurrentTestSpy);
+            assert.deepEqual(setCurrentTestSpy.thirdCall.args[0], {testPath: `${testFile}`, testTitle: secondTestTitle});
+            assert.equal(testResult.pass, false);
+
+            // Check the registry looks as expected
+            assert.deepEqual(snapshotTools.snapshotManager.registry, {
+                [`${testFile}`]: {
+                    [`${firstTestTitle}`]: 1,
+                    [`${secondTestTitle}`]: 2
+                }
+            });
+        });
+    });
 });
