e4b7a91a19d613de8bc8ed5985f732e7b499cbbd
Steve Larson <9larsons@gmail.com>
2026-02-20 15:21:54 -0600

    Enhanced unit tests in mw-vhost to use async/await and improve coverage checks
    - Updated test cases in vhost.test.js to utilize async/await for better readability and performance.
    - Modified the test:unit script in package.json to enforce 100% coverage checks.
    - Removed unnecessary dependencies from the test cases to streamline the testing process.

--- packages/mw-vhost/test/vhost.test.js
+++ packages/mw-vhost/test/vhost.test.js
@@ -1,10 +1,8 @@
 const assert = require('assert/strict');
-const http = require('http');
-const request = require('supertest');
 const vhost = require('..');
 
 describe('vhost(hostname, server)', function () {
-    it('should route by Host', function (done) {
+    it('should route by Host', async function () {
         const vhosts = [];
 
         vhosts.push(vhost('tobi.com', tobi));
@@ -19,13 +17,12 @@ describe('vhost(hostname, server)', function () {
             res.end('loki');
         }
 
-        request(app)
-            .get('/')
-            .set('Host', 'tobi.com')
-            .expect(200, 'tobi', done);
+        const response = await dispatch(app, {host: 'tobi.com'});
+        assert.equal(response.statusCode, 200);
+        assert.equal(response.body, 'tobi');
     });
 
-    it('should route by `req.hostname` (express v4)', function (done) {
+    it('should route by `req.hostname` (express v4)', async function () {
         const vhosts = [];
 
         vhosts.push(vhost('anotherhost.com', anotherhost));
@@ -43,35 +40,32 @@ describe('vhost(hostname, server)', function () {
             res.end('loki');
         }
 
-        request(app)
-            .get('/')
-            .set('Host', 'loki.com')
-            .expect(200, 'anotherhost', done);
+        const response = await dispatch(app, {host: 'loki.com'});
+        assert.equal(response.statusCode, 200);
+        assert.equal(response.body, 'anotherhost');
     });
 
-    it('should ignore port in Host', function (done) {
+    it('should ignore port in Host', async function () {
         const app = createServer('tobi.com', function (req, res) {
             res.end('tobi');
         });
 
-        request(app)
-            .get('/')
-            .set('Host', 'tobi.com:8080')
-            .expect(200, 'tobi', done);
+        const response = await dispatch(app, {host: 'tobi.com:8080'});
+        assert.equal(response.statusCode, 200);
+        assert.equal(response.body, 'tobi');
     });
 
-    it('should support IPv6 literal in Host', function (done) {
+    it('should support IPv6 literal in Host', async function () {
         const app = createServer('[::1]', function (req, res) {
             res.end('loopback');
         });
 
-        request(app)
-            .get('/')
-            .set('Host', '[::1]:8080')
-            .expect(200, 'loopback', done);
+        const response = await dispatch(app, {host: '[::1]:8080'});
+        assert.equal(response.statusCode, 200);
+        assert.equal(response.body, 'loopback');
     });
 
-    it('should 404 unless matched', function (done) {
+    it('should 404 unless matched', async function () {
         const vhosts = [];
 
         vhosts.push(vhost('tobi.com', tobi));
@@ -86,28 +80,18 @@ describe('vhost(hostname, server)', function () {
             res.end('loki');
         }
 
-        request(app)
-            .get('/')
-            .set('Host', 'ferrets.com')
-            .expect(404, done);
+        const response = await dispatch(app, {host: 'ferrets.com'});
+        assert.equal(response.statusCode, 404);
+        assert.equal(response.body, 'no vhost for "ferrets.com"');
     });
 
-    it('should 404 without Host header', function (done) {
+    it('should 404 without Host header', async function () {
         const vhosts = [];
 
         vhosts.push(vhost('tobi.com', tobi));
         vhosts.push(vhost('loki.com', loki));
 
-        const server = createServer(vhosts);
-        const listeners = server.listeners('request');
-
-        server.removeAllListeners('request');
-        listeners.unshift(function (req) {
-            req.headers.host = undefined;
-        });
-        listeners.forEach(function (l) {
-            server.addListener('request', l);
-        });
+        const app = createServer(vhosts);
 
         function tobi(req, res) {
             res.end('tobi');
@@ -116,9 +100,9 @@ describe('vhost(hostname, server)', function () {
             res.end('loki');
         }
 
-        request(server)
-            .get('/')
-            .expect(404, 'no vhost for "undefined"', done);
+        const response = await dispatch(app, {host: undefined});
+        assert.equal(response.statusCode, 404);
+        assert.equal(response.body, 'no vhost for "undefined"');
     });
 
     describe('arguments', function () {
@@ -152,51 +136,47 @@ describe('vhost(hostname, server)', function () {
     });
 
     describe('with string hostname', function () {
-        it('should support wildcards', function (done) {
+        it('should support wildcards', async function () {
             const app = createServer('*.ferrets.com', function (req, res) {
                 res.end('wildcard!');
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'loki.ferrets.com')
-                .expect(200, 'wildcard!', done);
+            const response = await dispatch(app, {host: 'loki.ferrets.com'});
+            assert.equal(response.statusCode, 200);
+            assert.equal(response.body, 'wildcard!');
         });
 
-        it('should restrict wildcards to single part', function (done) {
+        it('should restrict wildcards to single part', async function () {
             const app = createServer('*.ferrets.com', function (req, res) {
                 res.end('wildcard!');
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'foo.loki.ferrets.com')
-                .expect(404, done);
+            const response = await dispatch(app, {host: 'foo.loki.ferrets.com'});
+            assert.equal(response.statusCode, 404);
+            assert.equal(response.body, 'no vhost for "foo.loki.ferrets.com"');
         });
 
-        it('should treat dot as a dot', function (done) {
+        it('should treat dot as a dot', async function () {
             const app = createServer('a.b.com', function (req, res) {
                 res.end('tobi');
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'aXb.com')
-                .expect(404, done);
+            const response = await dispatch(app, {host: 'aXb.com'});
+            assert.equal(response.statusCode, 404);
+            assert.equal(response.body, 'no vhost for "aXb.com"');
         });
 
-        it('should match entire string', function (done) {
+        it('should match entire string', async function () {
             const app = createServer('.com', function (req, res) {
                 res.end('commercial');
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'foo.com')
-                .expect(404, done);
+            const response = await dispatch(app, {host: 'foo.com'});
+            assert.equal(response.statusCode, 404);
+            assert.equal(response.body, 'no vhost for "foo.com"');
         });
 
-        it('should populate req.vhost', function (done) {
+        it('should populate req.vhost', async function () {
             const app = createServer('user-*.*.com', function (req, res) {
                 const keys = Object.keys(req.vhost).sort();
                 const arr = keys.map(function (k) {
@@ -205,26 +185,24 @@ describe('vhost(hostname, server)', function () {
                 res.end(JSON.stringify(arr));
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'user-bob.foo.com:8080')
-                .expect(200, '[["0","bob"],["1","foo"],["host","user-bob.foo.com:8080"],["hostname","user-bob.foo.com"],["length",2]]', done);
+            const response = await dispatch(app, {host: 'user-bob.foo.com:8080'});
+            assert.equal(response.statusCode, 200);
+            assert.equal(response.body, '[["0","bob"],["1","foo"],["host","user-bob.foo.com:8080"],["hostname","user-bob.foo.com"],["length",2]]');
         });
     });
 
     describe('with RegExp hostname', function () {
-        it('should match using RegExp', function (done) {
+        it('should match using RegExp', async function () {
             const app = createServer(/[tl]o[bk]i\.com/, function (req, res) {
                 res.end('tobi');
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'toki.com')
-                .expect(200, 'tobi', done);
+            const response = await dispatch(app, {host: 'toki.com'});
+            assert.equal(response.statusCode, 200);
+            assert.equal(response.body, 'tobi');
         });
 
-        it('should match entire hostname', function (done) {
+        it('should match entire hostname', async function () {
             const vhosts = [];
 
             vhosts.push(vhost(/\.tobi$/, tobi));
@@ -239,13 +217,12 @@ describe('vhost(hostname, server)', function () {
                 res.end('loki');
             }
 
-            request(app)
-                .get('/')
-                .set('Host', 'loki.tobi.com')
-                .expect(404, done);
+            const response = await dispatch(app, {host: 'loki.tobi.com'});
+            assert.equal(response.statusCode, 404);
+            assert.equal(response.body, 'no vhost for "loki.tobi.com"');
         });
 
-        it('should populate req.vhost', function (done) {
+        it('should populate req.vhost', async function () {
             const app = createServer(/user-(bob|joe)\.([^.]+)\.com/, function (req, res) {
                 const keys = Object.keys(req.vhost).sort();
                 const arr = keys.map(function (k) {
@@ -254,10 +231,9 @@ describe('vhost(hostname, server)', function () {
                 res.end(JSON.stringify(arr));
             });
 
-            request(app)
-                .get('/')
-                .set('Host', 'user-bob.foo.com:8080')
-                .expect(200, '[["0","bob"],["1","foo"],["host","user-bob.foo.com:8080"],["hostname","user-bob.foo.com"],["length",2]]', done);
+            const response = await dispatch(app, {host: 'user-bob.foo.com:8080'});
+            assert.equal(response.statusCode, 200);
+            assert.equal(response.body, '[["0","bob"],["1","foo"],["host","user-bob.foo.com:8080"],["hostname","user-bob.foo.com"],["length",2]]');
         });
     });
 });
@@ -267,7 +243,7 @@ function createServer(hostname, server, pretest) {
         ? [vhost(hostname, server)]
         : hostname;
 
-    return http.createServer(function onRequest(req, res) {
+    return function onRequest(req, res) {
         // This allows you to perform changes to the request/response
         // objects before our assertions
         if (pretest) {
@@ -289,5 +265,29 @@ function createServer(hostname, server, pretest) {
         }
 
         next();
+    };
+}
+
+function dispatch(app, {host}) {
+    return new Promise((resolve, reject) => {
+        const req = {
+            headers: {host}
+        };
+
+        const res = {
+            statusCode: 200,
+            end(body) {
+                resolve({
+                    statusCode: this.statusCode,
+                    body: body || ''
+                });
+            }
+        };
+
+        try {
+            app(req, res);
+        } catch (err) {
+            reject(err);
+        }
     });
 }
