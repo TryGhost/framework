d0cb941cc8771fd2066c29e97a74ffd504070875
Austin Burdine <austin@ghost.org>
2026-08-17 15:49:40 -0400

    Fixed type exports in metrics + logging packages (#913)

--- packages/logging/lib/logging.d.ts
+++ packages/logging/lib/logging.d.ts
@@ -1,11 +1,11 @@
-import GhostLogger = require('./GhostLogger');
+import GhostLoggerClass = require('./GhostLogger');
 
 /**
  * A pre-configured `GhostLogger` instance (built from `loggingrc` if present),
  * with the `GhostLogger` class attached for creating additional instances.
  */
-declare const logging: GhostLogger & {
-    GhostLogger: typeof GhostLogger;
+declare const logging: GhostLoggerClass & {
+    GhostLogger: typeof GhostLoggerClass;
     /**
      * Clear the cached process-wide logger instance. Intended for tests only.
      */
@@ -13,13 +13,18 @@ declare const logging: GhostLogger & {
 };
 
 declare namespace logging {
-    export type LogLevel = GhostLogger.LogLevel;
-    export type Transport = GhostLogger.Transport;
-    export type RotationOptions = GhostLogger.RotationOptions;
-    export type ElasticsearchOptions = GhostLogger.ElasticsearchOptions;
-    export type GelfOptions = GhostLogger.GelfOptions;
-    export type HttpOptions = GhostLogger.HttpOptions;
-    export type GhostLoggerOptions = GhostLogger.GhostLoggerOptions;
+    /**
+     * Instance type of the `GhostLogger` class, so consumers can annotate
+     * values with `GhostLogger` without reaching for `InstanceType<>`.
+     */
+    export type GhostLogger = GhostLoggerClass;
+    export type LogLevel = GhostLoggerClass.LogLevel;
+    export type Transport = GhostLoggerClass.Transport;
+    export type RotationOptions = GhostLoggerClass.RotationOptions;
+    export type ElasticsearchOptions = GhostLoggerClass.ElasticsearchOptions;
+    export type GelfOptions = GhostLoggerClass.GelfOptions;
+    export type HttpOptions = GhostLoggerClass.HttpOptions;
+    export type GhostLoggerOptions = GhostLoggerClass.GhostLoggerOptions;
 }
 
 export = logging;
